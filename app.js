1a438ca7bc273d58076b1e86a4c684f4288c1345
Alessandro Catter-Martinez <alessandro.cattermartinez@gmail.com>
2016-05-30 13:37:43 -0400

    Parameter overrides.

--- app.js
+++ app.js
@@ -2,11 +2,20 @@
 
 import './bootstrap';
 import Class2 from './lib/class2';
+import Class1 from './lib/class1';
+import Class0 from './lib/class0';
 
-const a = new Class2();
+const x = false;
+const y = new Class0();
+const z = new Class1();
+
+const a = new Class2(z,y,x);
 const b = new Class2();
+const c = new Class2(undefined,y);
 
-console.log('a: ', JSON.stringify(a._class1));
+console.log('a: ', JSON.stringify(a));
 console.log('a instanceof Class2: ', a instanceof Class2);
-console.log('b: ', JSON.stringify(b._class1));
+console.log('b: ', JSON.stringify(b));
 console.log('b instance of Class2: ', b instanceof Class2);
+console.log('c: ', JSON.stringify(c));
+console.log('c instance of Class2: ', c instanceof Class2);
